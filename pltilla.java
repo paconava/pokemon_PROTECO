aa5d2dd95d516f8374ecb5f26306c0aff1dc08d2
Saúl Axel Martínez Ortiz <saulaxel.50@gmail.com>
2016-10-02 15:21:44 -0500

    Añadida la plantilla en que trabajaremos

---
+++ pltilla.java
@@ -0,0 +1,20 @@
+/*
+ * Universidad Nacional Autonoma de MExico
+ * IngenierIa en computaciOn
+ * Integrantes:
+ *  MartInez Ortiz SaUl Axel
+ *  Nava Luján Francisco José
+ * PROTECO, generaciOn 33
+ * Java bAsico, proyecto 1
+ * FECHA DD/MM/AAAA
+ * DescripciOn: VersiOn en java del juego de pokemon
+ *  sin interfaz grafica
+ * Recursos o fuentes:
+
+ */
+
+//import java.util.Scanner;
+
+public class <nombre_clase>{
+
+}
